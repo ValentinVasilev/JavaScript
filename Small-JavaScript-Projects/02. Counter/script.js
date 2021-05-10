02045cd8547a139925137e1b9afae7af0852cfed
ValentinVasilev <shirleymay@abv.bg>
2021-05-11 01:33:21 +0300

    Update script.js

--- Small-JavaScript-Projects/02. Counter/script.js
+++ Small-JavaScript-Projects/02. Counter/script.js
@@ -1,39 +1,32 @@
-let btnDecrease = document.getElementById('decrease');
-let btnReset = document.getElementById('reset');
-let btnIncrease = document.getElementById('increase');
-let display = document.getElementById('displayBox');
+let btnDecrease = document.getElementById("decrease");
+let btnReset = document.getElementById("reset");
+let btnIncrease = document.getElementById("increase");
+let display = document.getElementById("displayBox");
 
 let counter = 0;
 
+btnDecrease.addEventListener("click", () => {
+  counter--;
+  display.innerHTML = counter;
+  positiveOrNegative();
+});
 
+btnReset.addEventListener("click", () => {
+  counter = 0;
+  display.innerHTML = counter;
+  display.style.color = "green";
+});
 
-btnDecrease.addEventListener('click', () => {
-    counter--;
-    display.innerHTML = counter;
-    positiveOrNegative();
-    
-})
+btnIncrease.addEventListener("click", () => {
+  counter++;
+  display.innerHTML = counter;
+  positiveOrNegative();
+});
 
-btnReset.addEventListener('click', () => {
-    counter = 0;
-    display.innerHTML = counter;
-    display.style.color = 'green';
-    
-})
-
-btnIncrease.addEventListener('click', () => {
-    counter++;
-    display.innerHTML = counter;
-    positiveOrNegative();
-    
-
-})
-
-function positiveOrNegative(){
-
-    if(counter % 2 == 0){
-        display.style.color = 'blue';
-    }else{
-        display.style.color = 'red';
-    }
+function positiveOrNegative() {
+  if (counter % 2 == 0) {
+    display.style.color = "blue";
+  } else {
+    display.style.color = "red";
+  }
 }
