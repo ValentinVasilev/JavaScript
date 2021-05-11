2d82a17de3a6a8c2f817c4dbb1532161952f5bde
ValentinVasilev <shirleymay@abv.bg>
2021-05-11 22:11:36 +0300

    Update script.js
    Add Comments to the key points in the code.

--- Small-JavaScript-Projects/03. TipCalculator/script.js
+++ Small-JavaScript-Projects/03. TipCalculator/script.js
@@ -1,23 +1,30 @@
+// Get the input fields, submite button and display section
 let priceInput = document.getElementById("price");
 let tips = document.getElementById("tip");
 let peopleCount = document.getElementById("numberOfPeople");
 let btnSubmit = document.getElementById("btn");
 let display = document.getElementById("display");
 
+//Add event to the submite button.
 btnSubmit.addEventListener("click", (e) => {
   e.preventDefault();
 
+  //Check if input fields are empty or negative numbers.
   if (priceInput.value <= 0) {
+      display.style.color = 'red';
     display.innerHTML = "Невалидна сума.";
     return;
   } else if (!Number(tips.value)) {
+    display.style.color = 'red';
     display.innerHTML = "Невалиден бакшиш.";
     return;
-  } else if (!peopleCount.value) {
+  } else if (peopleCount.value <= 0) {
+    display.style.color = 'red';
     display.innerHTML = "Невалиден брой хора.";
     return;
   }
 
+  //Calculate tip
   let tipsCalculate = Number(priceInput.value * tips.value);
   let tipPerPerson = tipsCalculate / peopleCount.value;
   display.innerHTML = tipPerPerson.toFixed(2) + "лв. на човек.";
