7e55956407a6983a8e8257f6612624187d0469c4
ValentinVasilev <shirleymay@abv.bg>
2021-05-11 22:06:26 +0300

    Completed.
    Tip calculator fully completed.

--- Small-JavaScript-Projects/03. TipCalculator/script.js
+++ Small-JavaScript-Projects/03. TipCalculator/script.js
@@ -1,1 +1,24 @@
-console.log('Hello');
+let priceInput = document.getElementById("price");
+let tips = document.getElementById("tip");
+let peopleCount = document.getElementById("numberOfPeople");
+let btnSubmit = document.getElementById("btn");
+let display = document.getElementById("display");
+
+btnSubmit.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (priceInput.value <= 0) {
+    display.innerHTML = "Невалидна сума.";
+    return;
+  } else if (!Number(tips.value)) {
+    display.innerHTML = "Невалиден бакшиш.";
+    return;
+  } else if (!peopleCount.value) {
+    display.innerHTML = "Невалиден брой хора.";
+    return;
+  }
+
+  let tipsCalculate = Number(priceInput.value * tips.value);
+  let tipPerPerson = tipsCalculate / peopleCount.value;
+  display.innerHTML = tipPerPerson.toFixed(2) + "лв. на човек.";
+});
