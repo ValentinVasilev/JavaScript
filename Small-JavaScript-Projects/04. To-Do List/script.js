50820131da531957688bda7a2e8a1ff5105a82c3
ValentinVasilev <shirleymay@abv.bg>
2021-05-14 12:14:03 +0300

    Update script.js
    Add comments to the important parts of the code.

--- Small-JavaScript-Projects/04. To-Do List/script.js
+++ Small-JavaScript-Projects/04. To-Do List/script.js
@@ -1,36 +1,40 @@
-let inputField = document.getElementById('inputField');
-let addButton = document.getElementById('AddBtn');
-let getDisplay = document.getElementById('display');
+//Get the inputField, addButton and display section
+let inputField = document.getElementById("inputField");
+let addButton = document.getElementById("AddBtn");
+let getDisplay = document.getElementById("display");
 
+//Add event to AddButton
+addButton.addEventListener("click", () => {
+  //Create 'p' element and fill it up with info from input field.
+  let createPelement = document.createElement("p");
+  createPelement.innerHTML = (inputField.value + " ").trimStart();
+  createPelement.classList.add("list");
 
-addButton.addEventListener('click', () => {
-    let createPelement = document.createElement('p');
-    createPelement.innerHTML = (inputField.value + ' ').trimStart();
-    createPelement.classList.add('list');
+  //Create 'ul' element.
+  let createUl = document.createElement("ul");
+  createUl.appendChild(createPelement);
 
-    let createUl = document.createElement('ul');
-    createUl.appendChild(createPelement);
+  // Append 'p' and 'ul' to Display section
+  document.querySelector("#display").appendChild(createUl);
+  getDisplay.appendChild(createUl);
 
-    document.querySelector('#display').appendChild(createUl);
-    
+  //create 'Delete' button and append it to 'p' element.
+  let createBtn = document.createElement("button");
+  createBtn.innerHTML = "X";
+  createBtn.classList.add("clear");
+  createPelement.appendChild(createBtn);
 
-    getDisplay.appendChild(createUl);
+  //Clear the data from input field;
+  inputField.value = "";
 
-    let createBtn = document.createElement('button');
-    createBtn.innerHTML = 'X';
-    createBtn.classList.add('clear');
+ //Remove the 'p' element on clicking the 'X' button.
+  createBtn.addEventListener("click", () => {
+    createUl.remove();
+  });
 
-    createPelement.appendChild(createBtn);
-
-    inputField.value = '';
-    createBtn.addEventListener('click', () => {
-        createUl.remove();
-    })
-
-    createPelement.addEventListener('click', () => {
-        createPelement.style.backgroundColor = 'green';
-        createBtn.style.backgroundColor = 'red';
-    })
+  //Chanage 'p' element color and 'X' button color on clicking the 'p' element.
+  createPelement.addEventListener("click", () => {
+    createPelement.style.backgroundColor = "green";
+    createBtn.style.backgroundColor = "red";
+  });
 });
-
-
